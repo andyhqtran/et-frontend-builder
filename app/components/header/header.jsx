9ea79731dc106640f1e71b1ef73128315b1e2d1b
Andy Tran <andyhqtran@gmail.com>
2016-04-04 16:04:50 -0700

    Updated header component.

--- app/components/header/header.jsx
+++ app/components/header/header.jsx
@@ -1,14 +1,10 @@
 var React = require('react');
 
-var ETBuilderButton = require('../button/button')
-
 var ETBuilderHeader = React.createClass({
 	render: function() {
 		return (
 			<div className="et-fb-header">
-        <ETBuilderButton type="button" modifier="success">Save</ETBuilderButton>
-        <ETBuilderButton type="button" modifier="primary">Save</ETBuilderButton>
-				<ETBuilderButton type="button" modifier="inverse">Save</ETBuilderButton>
+        Header
 			</div>
 		);
 	}
