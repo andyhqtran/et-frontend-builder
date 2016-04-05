9e1af6920d27be290eb038d30a193eab0c1bbeea
Andy Tran <andyhqtran@gmail.com>
2016-04-05 11:35:58 -0700

    Updated form button component.

--- app/components/forms/form-button/form-button.jsx
+++ app/components/forms/form-button/form-button.jsx
@@ -1,13 +1,24 @@
 var React = require('react');
+var classNames = require('classnames');
 
 var ETBuilderFormButton = React.createClass({
   propTypes: {
-    modifier: React.PropTypes.string,
-    type: React.PropTypes.string
+    children: React.PropTypes.node,
+    inverse: React.PropTypes.bool,
+    primary: React.PropTypes.bool,
+    success: React.PropTypes.bool,
+    type: React.PropTypes.string.isRequired
   },
   render: function() {
+    var classes = classNames({
+      'et-fb-form-button': true,
+      'et-fb-form-button--inverse': this.props.inverse,
+      'et-fb-form-button--primary': this.props.primary,
+      'et-fb-form-button--success': this.props.success
+    });
+
     return (
-      <button type={this.props.type} className={this.props.modifier ? 'et-fb-form-button et-fb-form-button--' + this.props.modifier : 'et-fb-form-button'}>
+      <button type={this.props.type} className={classes}>
         {this.props.children}
       </button>
     )
