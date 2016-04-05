f30db5ceb2dae60cb28d394674dd5cba569f9f60
Andy Tran <andyhqtran@gmail.com>
2016-04-05 15:02:43 -0700

    Added in form toggle component state, onChange function.

--- app/components/forms/form-toggle/form-toggle.jsx
+++ app/components/forms/form-toggle/form-toggle.jsx
@@ -2,15 +2,34 @@ var React = require('react');
 var classNames = require('classnames');
 
 var ETBuilderFormToggle = React.createClass({
+  getInitialState: function() {
+    return {
+      value: this.props.value
+    }
+  },
+  getDefaultProps: function() {
+    return {
+      value: 'off'
+    }
+  },
+  _onChange: function(event) {
+    var newValue = this.state.value === 'on' ? 'off' : 'on';
+
+    // this.props._onChange(this.props.name, newValue);
+
+    this.setState({
+      value: newValue
+    });
+  },
   render: function() {
     var classes = classNames({
       'et-fb-form-toggle': true,
-      'et-fb-form-toggle--on': this.props.value === 'on',
-      'et-fb-form-toggle--off': this.props.value === 'off' || !this.props.value
+      'et-fb-form-toggle--on': this.state.value === 'on',
+      'et-fb-form-toggle--off': this.state.value === 'off' || !this.state.value
     });
 
     return (
-        <div className={classes}>
+        <div className={classes} onClick={this._onChange}>
           <div className="et-fb-form-toggle__label et-fb-form-toggle__label--on">Yes</div>
           <div className="et-fb-form-toggle__label et-fb-form-toggle__label--off">No</div>
           <div className="et-fb-form-toggle__handle"></div>
